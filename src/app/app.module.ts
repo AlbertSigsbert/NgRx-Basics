f6aa8325ed8dbdab81bea3f7776908e232791e72
Albert Sigsbert <ishengomaalbert@gmail.com>
2023-07-17 06:39:54 +0300

    Adding a Reducer & Store Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { CounterOutputComponent } from './counter-output/counter-output.component';
 import { CounterControlsComponent } from './counter-controls/counter-controls.component';
-import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [

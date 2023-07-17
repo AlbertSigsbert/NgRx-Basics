c620cb7e525fbb4668979ec2c9396724821ab72d
Albert Sigsbert <ishengomaalbert@gmail.com>
2023-07-17 13:31:29 +0300

    Dispatching Actions

--- src/app/counter-controls/counter-controls.component.ts
+++ src/app/counter-controls/counter-controls.component.ts
@@ -1,20 +1,17 @@
 import { Component } from '@angular/core';
-
-import { CounterService } from '../counter.service';
-
+import { Store } from '@ngrx/store';
+import { increment } from '../store/counter.actions';
 @Component({
   selector: 'app-counter-controls',
   templateUrl: './counter-controls.component.html',
   styleUrls: ['./counter-controls.component.css'],
 })
 export class CounterControlsComponent {
-  constructor(private counterService: CounterService) {}
+  constructor(private store: Store) {}
 
   increment() {
-    this.counterService.increment();
+    this.store.dispatch(increment());
   }
 
-  decrement() {
-    this.counterService.decrement();
-  }
+  decrement() {}
 }

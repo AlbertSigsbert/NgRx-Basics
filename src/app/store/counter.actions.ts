763ffad0ad1be2051497631fa6fd1963180e6636
Albert Sigsbert <ishengomaalbert@gmail.com>
2023-07-18 12:14:50 +0300

    Decrement Action

--- src/app/store/counter.actions.ts
+++ src/app/store/counter.actions.ts
@@ -4,4 +4,10 @@ import { createAction, props} from "@ngrx/store";
 export const increment = createAction(
     '[Counter] Increment',
      props<{value:number}>()
+);
+
+
+export const decrement = createAction(
+    '[Counter] Decrement',
+    props<{value:number}>()
 )

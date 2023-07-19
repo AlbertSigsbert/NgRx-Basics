d14b3ca5bb1d6001bacdf8c03d0b66b6a9e1ce45
Albert Sigsbert <ishengomaalbert@gmail.com>
2023-07-19 12:01:44 +0300

    Counter Effects

---
+++ src/app/store/counter.effects.ts
@@ -0,0 +1,21 @@
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+
+import { tap } from 'rxjs';
+
+import { increment, decrement } from './counter.actions';
+
+export class CounterEffects {
+  constructor(private actions$: Actions) {}
+
+  saveCount = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(increment, decrement),
+        tap((action) => {
+          console.log(action);
+          localStorage.setItem('count', action.value.toString());
+        })
+      ),
+    { dispatch: false }
+  );
+}

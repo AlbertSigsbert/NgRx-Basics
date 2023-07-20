51eb5efbc0371a7fe69f8129061ecd503f97984a
Albert Sigsbert <ishengomaalbert@gmail.com>
2023-07-20 07:05:27 +0300

    Using stofe data in Effects

--- src/app/store/counter.effects.ts
+++ src/app/store/counter.effects.ts
@@ -1,21 +1,24 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 
-import { tap } from 'rxjs';
+import { tap, withLatestFrom } from 'rxjs';
 
 import { increment, decrement } from './counter.actions';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectCount } from './counter.selectors';
 
 @Injectable()
 export class CounterEffects {
-  constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions, private store:Store<{counter:number}>) {}
 
   saveCount = createEffect(
     () =>
       this.actions$.pipe(
         ofType(increment, decrement),
-        tap((action) => {
+        withLatestFrom(this.store.select(selectCount)),
+        tap(([action, counter]) => {
           console.log(action);
-          localStorage.setItem('count', action.value.toString());
+          localStorage.setItem('count', counter.toString());
         })
       ),
     { dispatch: false }
